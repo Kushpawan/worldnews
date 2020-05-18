8e4485458c013e15926915777cfb5352d174661a
Pawan Kushwaha <pavan.guddu@gmail.com>
2020-05-18 17:06:08 +0530

    project cretaed.

---
+++ app/src/main/java/com/example/worldnews/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.worldnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    val apiKey ="a0a21a67f3b24f4696d67341ab342d54"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
